63fd3b0e63628e915596797d84e8517b9a6f26df
Andreea Pantea <andreea.pantea@fxbits.io>
2022-06-04 17:21:15 +0300

    refactor

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number!';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
-
 //state variable(because is displayed in the state of the application);
 let score = 20;
 let highScore = 0;
