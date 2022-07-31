3523996678f50bc2cae0c1910b5b1122efc1064a
Andreea Pantea <andreea.pantea@fxbits.io>
2022-07-31 16:10:51 +0300

    optional chain

--- Arrays/script.js
+++ Arrays/script.js
@@ -3,6 +3,23 @@
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+ 
+  const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+  const openingHours = {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekdays[5]]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  
+  }
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,51 +29,68 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-   openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  
-  },
+  //ES6 enhanced object literals
+  //1
+  openingHours,
 
-  order: function(starterIndex, mainIndex){
+  //2
+  order(starterIndex, mainIndex){
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
   },
 
-  orderDelivery: function({time, address, mainIndex, starterIndex}){
+  //3
+  orderDelivery({time, address, mainIndex, starterIndex}){
       // console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
 
-  orderPasta: function(ing1, ing2, ing3){
+  //4
+  orderPasta(ing1, ing2, ing3){
       console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function(mainIngredient, ...otherIngredients){{
+  //5
+  orderPizza(mainIngredient, ...otherIngredients){{
     console.log('mainIngredient', mainIngredient);
     console.log('otherIngredients', otherIngredients);
   }}
-};
+  };
 
-//Logical assignment operators
-const res1 = {
-  name: 'Capri',
-  // numGuests: 20
-  numGuests: 0
-}
+//Optional Chaining (?)
+// for(const day of weekdays){
+  // const open = restaurant.openingHours[day]?.open ?? 'closed';
+  // console.log(`On ${day}, we open at ${open}`)
+// }
 
-const res2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi'
-}
+//optional chaining on METHODS
+// console.log(restaurant.order?.(0,1) ?? "Method does not exist");
+// console.log(restaurant.orderRisotto?.[0,1] ?? "Method does not exist");
+
+//optional chaining on ARRAYS
+const users = [{
+  name: 'Jonas',
+  email: 'hello@jonas.io'
+}];
+console.log(users[0]?.name ?? 'User array empty');
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for(const item of menu) console.log(item);
+
+// for(const [i,el] of menu.entries()){
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// //Logical assignment operators
+// const res1 = {
+//   name: 'Capri',
+//   // numGuests: 20
+//   numGuests: 0
+// }
+
+// const res2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi'
+// }
 
 //OR assignment operator
 // res1.numGuests = res1.numGuests || 10;
@@ -74,8 +108,8 @@ const res2 = {
 
 // res1.owner &&='<ANONYMOUS>' 
 // res2.owner &&='<ANONYMOUS>' 
-console.log(res1);
-console.log(res2);
+// console.log(res1);
+// console.log(res2);
 
 // NULLISH: null and undefined (NOT 0 or '')
 // restaurant.numGuests = 0;
