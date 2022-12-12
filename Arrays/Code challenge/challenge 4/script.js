3ecb926e4b6af325a348cb7dff504993673c36aa
Andreea Pantea <andreea.pantea@fxbits.io>
2022-12-13 00:17:59 +0200

    functions

---
+++ Arrays/Code challenge/challenge 4/script.js
@@ -0,0 +1,35 @@
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+
+// document.querySelector('button').addEventListener('click', function(){
+//     const text = document.querySelector('textarea').value;
+//     const rows = text.split('\n');
+
+//     for(const [i,row] of rows.entries()){
+//         const [first, second] = row.toLowerCase().trim().split('_');
+//         const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+//         console.log(`${output.padEnd(20)}${'!'.repeat(i + 1)}`);
+//     }
+       
+// })
+
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+
+
+
+const getCode = str => str.slice(0,3).toUpperCase();
+
+for(const flight of flights.split('+')){
+    // created lines with the flight string
+    // console.log(flight.split(';'))
+    //using destructuring
+    const [type, from, to, time] = flight.split(';')
+    const output = `${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} ${time.replace(':', 'h')}`.padStart(36)
+    console.log(output);
+}
+ 
